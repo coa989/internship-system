f2fcc7e66889b97f270b237e401bc8aef5dbce8a
coa989 <a.marjanovic989@gmail.com>
2021-09-21 12:21:11 +0200

    create db connection, add dotenv package

--- index.php
+++ index.php
@@ -3,4 +3,12 @@ require_once __DIR__. '/vendor/autoload.php';
 
 use app\db\Database;
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $db = new Database();
+
+$stmt = $db->pdo->prepare('SELECT * FROM mentors');
+$stmt->execute();
+
+var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
