c3e0e4a2914df8a5293d7b504e5273fafeb50f3a
coa989 <a.marjanovic989@gmail.com>
2021-09-21 12:51:50 +0200

    create seed data with phinx and faker

---
+++ db/seeds/GroupSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class GroupSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 5; $i++) {
+            $data[] = [
+                'name' => $faker->name
+            ];
+        }
+        
+        $this->table('groups')->insert($data)->saveData();
+    }
+}
