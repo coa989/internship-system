6f0ee77000382d2cb7d92b0473b19adc1d9c075d
coa989 <a.marjanovic989@gmail.com>
2021-09-22 09:52:45 +0200

    update sql file and seed data

--- db/database.sql
+++ db/database.sql
@@ -10,7 +10,10 @@ CREATE TABLE `internship_system`.`mentors` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
     `first_name` VARCHAR(255) NOT NULL ,
     `last_name` VARCHAR(255) NOT NULL ,
+    `email` VARCHAR(255) NOT NULL,
     `group_id` BIGINT UNSIGNED NOT NULL ,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`),
     PRIMARY KEY (`id`))
     ENGINE = InnoDB;
@@ -19,7 +22,10 @@ CREATE TABLE `internship_system`.`interns` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT ,
     `first_name` VARCHAR(255) NOT NULL ,
     `last_name` VARCHAR(255) NOT NULL ,
+    `email` VARCHAR(255) NOT NULL,
     `group_id` BIGINT UNSIGNED NOT NULL ,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`),
     PRIMARY KEY (`id`))
     ENGINE = InnoDB;
