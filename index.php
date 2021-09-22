fe34e2e7f51490fdae7c2d50ae1da73b852213dd
coa989 <a.marjanovic989@gmail.com>
2021-09-22 11:49:59 +0200

    add store, update, destroy mentor functionality

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
 
-use app\src\Mentor;
-
 require_once __DIR__. '/vendor/autoload.php';
-
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $router = new Bramus\Router\Router();
 
-$router->get('/api/mentors', function() {
-    echo (new Mentor())->index();
-});
+$router->setNamespace('\app\src');
 
-$router->get('/api/mentors/(\d+)', function($id) {
-    echo (new Mentor())->show($id);
-});
+// Mentor Routes
+$router->get('/api/mentors', 'Mentor@index');
+$router->get('/api/mentors/(\d+)', 'Mentor@show');
+$router->delete('/api/mentors/(\d+)/destroy', 'Mentor@destroy');
+$router->post('/api/mentors', 'Mentor@store');
+$router->post('/api/mentors/(\d+)/update', 'Mentor@update');
 
 $router->run();
--- src/Mentor.php
+++ src/Mentor.php
@@ -7,6 +7,7 @@ use app\db\Database;
 class Mentor
 {
     private Database $db;
+    private array $attributes = ['first_name', 'last_name', 'email', 'group_id'];
 
     public function __construct()
     {
@@ -17,7 +18,7 @@ class Mentor
     {
         $statement = $this->db->pdo->prepare("SELECT * FROM mentors ORDER BY created_at DESC ");
         $statement->execute();
-        return json_encode($statement->fetchAll(\PDO::FETCH_OBJ));
+        echo json_encode($statement->fetchAll(\PDO::FETCH_OBJ));
     }
 
     public function show($id)
@@ -25,6 +26,53 @@ class Mentor
         $statement = $this->db->pdo->prepare("SELECT * FROM mentors WHERE id=:id");
         $statement->bindValue(':id', $id);
         $statement->execute();
-        return json_encode($statement->fetch(\PDO::FETCH_OBJ));
+        $result = $statement->fetch(\PDO::FETCH_OBJ);
+
+        if (!$result) {
+            http_response_code(404);
+            echo 'Not Found!';
+            die();
+        }
+
+        echo json_encode($result);
+    }
+
+    public function store()
+    {
+        // todo validate data
+        $statement = $this->db->pdo->prepare("INSERT INTO mentors (first_name, last_name, email, group_id) VALUES (:first_name, :last_name, :email, :group_id)");
+        $statement->bindValue(':first_name', $_POST['first_name']);
+        $statement->bindValue(':last_name', $_POST['last_name']);
+        $statement->bindValue(':email', $_POST['email']);
+        $statement->bindValue(':group_id', $_POST['group_id']);
+        $statement->execute();
+
+        http_response_code(201);
+
+        echo 'Success';
+    }
+
+    public function update($id)
+    {
+        // todo validate data
+        $statement = $this->db->pdo->prepare("UPDATE mentors SET first_name=:first_name, last_name=:last_name, email=:email, group_id=:group_id WHERE id=:id");
+        $statement->bindValue(':id', $id);
+        $statement->bindValue(':first_name', $_POST['first_name']);
+        $statement->bindValue(':last_name', $_POST['last_name']);
+        $statement->bindValue(':email', $_POST['email']);
+        $statement->bindValue(':group_id', $_POST['group_id']);
+        $statement->execute();
+
+        http_response_code(200);
+
+        echo 'Success';
+    }
+
+    public function destroy($id)
+    {
+        $statement = $this->db->pdo->prepare("DELETE FROM mentors WHERE id=:id");
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+        return true;
     }
 }
